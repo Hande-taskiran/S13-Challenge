aedaee264196801293c5456b3bd08ad56df75251
Hande-taskiran <handetaskiranht@gmail.com>
2024-09-20 15:44:02 +0300

    tests passed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        run();
+    }
+
+    public static void run() {
+        HealthPlan basic = new HealthPlan(1, "Basic Health Plan", Plan.BASIC);
+        System.out.println(basic);
+
+        String[] healthPlanArray = new String[5];
+
+
+        Employee employee = new Employee(101, "Kaan Kaya", "kaan.kaya@example.com", "password123", healthPlanArray);
+        employee.addHealthPlan(0, basic.getName());
+        System.out.println(employee);
+
+        String[] developerNamesArray = new String[5];
+
+
+        Company company = new Company(50, "companyxx", 100, developerNamesArray);
+        company.addEmployee(0, "OlguTaşkıran");
+        System.out.println(company);
     }
 }
